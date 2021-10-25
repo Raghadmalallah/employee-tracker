b8537ff03a8664099c4b1775c89d777e25c81942
Raghad Malallah <raghadmalallah@gmail.com>
2021-10-24 23:11:35 -0700

    fixing sintax issues with the seeds file

--- server.js
+++ server.js
@@ -10,3 +10,27 @@ const connection = mysql.createConnection({
     database: 'employee_trackerdb'
 })
 
+connection.connect(function(err){
+    if (err) throw err;
+    options();
+})
+
+function options() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'Welcome to our employee database! What would you like to do?',
+            choices: [
+                    'View all employees',
+                    'View all departments',
+                    'View all roles',
+                    'Add an employee',
+                    'Add a department',
+                    'Add a role',
+                    'Update employee role',
+                    'Delete an employee',
+                    'EXIT'
+                    ]
+            })};
+            
