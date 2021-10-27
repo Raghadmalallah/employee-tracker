afaf28255c62b518882fb26a0accdb825c96c99d
Raghad Malallah <raghadmalallah@gmail.com>
2021-10-26 20:16:50 -0700

    adding rols

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
-
-
+const art = require('ascii-art');
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -10,15 +9,22 @@ const connection = mysql.createConnection({
     password: 'password',
     database: 'employee_trackerdb'
 })
+start();
+
+async function start() {
+    try {
+        const rendered = await art.font("---Employee Tracker---", "doom").completed();
+        console.log(rendered);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 connection.connect(function(err){
     if (err) throw err;
     initialAction();
 })
 
-console.table(
-    "\n------ * EMPLOYEE TRACKER *  ------\n"
-)
 const initialAction = async () => {
     try {
         let answer = await inquirer.prompt({
@@ -118,6 +124,150 @@ const roleView = async () => {
             console.table(roleArray);
             initialAction();
         });
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
+const employeeAdd = async () => {
+    try {
+        console.log('Employee Add');
+
+        let roles = await connection.query("SELECT * FROM role");
+
+        let managers = await connection.query("SELECT * FROM employee");
+
+        let answer = await inquirer.prompt([
+            {
+                name: 'firstName',
+                type: 'input',
+                message: 'What is the first name of this Employee?'
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: 'What is the last name of this Employee?'
+            },
+            {
+                name: 'employeeRoleId',
+                type: 'list',
+                choices: roles.map((role) => {
+                    return {
+                        name: role.title,
+                        value: role.id
+                    }
+                }),
+                message: "What is this Employee's role id?"
+            },
+            {
+                name: 'employeeManagerId',
+                type: 'list',
+                choices: managers.map((manager) => {
+                    return {
+                        name: manager.first_name + " " + manager.last_name,
+                        value: manager.id
+                    }
+                }),
+                message: "What is this Employee's Manager's Id?"
+            }
+        ])
+
+        let result = await connection.query("INSERT INTO employee SET ?", {
+            first_name: answer.firstName,
+            last_name: answer.lastName,
+            role_id: (answer.employeeRoleId),
+            manager_id: (answer.employeeManagerId)
+        });
+
+        console.log(`${answer.firstName} ${answer.lastName} added successfully.\n`);
+        initialAction();
+
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
+
+const departmentAdd = async () => {
+    try {
+        console.log('Department Add');
+
+        let answer = await inquirer.prompt([
+            {
+                name: 'deptName',
+                type: 'input',
+                message: 'What is the name of your new department?'
+            }
+        ]);
+
+
+        console.log(`${answer.deptName} added successfully to departments.\n`)
+        initialAction();
+
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
+
+const roleAdd = async () => {
+    try {
+        console.log('Role Add');
+
+        let departments = await connection.query("SELECT * FROM department")
+
+        let answer = await inquirer.prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'What is the name of your new role?'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What salary will this role provide?'
+            },
+            {
+                name: 'departmentId',
+                type: 'list',
+                choices: departments.map((departmentId) => {
+                    return {
+                        name: departmentId.department_name,
+                        value: departmentId.id
+                    }
+                }),
+                message: 'What department ID is this role associated with?',
+            }
+        ]);
+        
+        for (i = 0; i < departments.length; i++) {
+            if(departments[i].department_id === answer.choice) {
+            };
+        }
+
+        console.log(`${answer.title} role added successfully.\n`)
+        initialAction();
+
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
+
+const employeeUpdate = async () => {
+    try {
+        console.log('Employee Update');
+        
+        let employees = await connection.query("SELECT * FROM employee");
+
+
+        let roles = await connection.query("SELECT * FROM role");
+
+
+
+        console.log(`The role was successfully updated.\n`);
+        initialAction();
+
     } catch (err) {
         console.log(err);
         initialAction();
